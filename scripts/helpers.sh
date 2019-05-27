63f0de10e614e798226e3fc0d0724eb55138354d
Todd Yamakawa <todd.yamakawa@gmail.com>
2019-05-27 09:58:23 -0500

    helpers.sh: Adding function to get tmux option.

--- scripts/helpers.sh
+++ scripts/helpers.sh
@@ -1,4 +1,13 @@
 #!bash
-declare -r CURRENT_DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
+declare -r HELPERS_DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
 
+function tmux-get-option() {
+	local option="$1" default_value="$2" option_value
+	option_value=$(tmux show-option -gqv "$option")
+	if [[ -z "$option_value" ]]; then
+		echo "$default_value"
+	else
+		echo "$option_value"
+	fi
+}
 
