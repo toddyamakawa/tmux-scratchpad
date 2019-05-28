e04b84665c84e630e5b4accdb83203b2a64d70ca
Todd Yamakawa <todd.yamakawa@gmail.com>
2019-05-28 05:24:06 -0500

    scripts: scratch_cmd.bash: Updating command prompt string.

--- scripts/scratch_cmd.bash
+++ scripts/scratch_cmd.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 declare -r CURRENT_DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
 
-tmux command-prompt -p "Enter command:" \
+tmux command-prompt -p "[scratchpad] Enter command:" \
 	"run-shell -b '${CURRENT_DIR}/scratch_pane.bash \"%%\"'"
 
