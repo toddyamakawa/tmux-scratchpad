793c57c1e4a98b86314e024e52e68143cfaa72e8
Todd Yamakawa <todd.yamakawa@arm.com>
2020-06-08 21:35:11 -0500

    scripts/scratch_pane.bash: Actually using hooks.

--- scripts/scratch_pane.bash
+++ scripts/scratch_pane.bash
@@ -15,10 +15,9 @@ function tmux-print() {
 # Parse args
 cmd="$@"
 if [[ -z $cmd ]]; then
-	tmux display-message 'scratchpad: command missing'
+	tmux display-message 'scratchpad: command not specified'
 	exit
 fi
-cmd=$(eval tmux-print "'$@'")
 
 # Get pane information
 # TODO: Get scroll position
@@ -26,6 +25,13 @@ read -r pane_id pane_height mode scroll_pos <<<$(
 	tmux display-message -p \
 		'#{pane_id} #{pane_height} #{?pane_in_mode,1,0} #{scroll_position}'
 )
+
+# Check command
+if [[ "$cmd" == 'pager' ]]; then
+	cmd="tmux capture-pane -e -J -p -t '$pane_id' -S - | less -r +G"
+else
+	cmd=$(eval tmux-print "'$@'")
+fi
 
 # Create new window for scratchpad
 scratch_name="[scratch-$pane_id]"
@@ -36,7 +42,7 @@ read -r scratch_window_id scratch_pane_id <<<$(
 )
 
 # Close tmux pane on exit
-tmux set-option -t "$scratch_pane_id" remain-on-exit on
+tmux set-window-option -t "$scratch_pane_id" remain-on-exit on
 tmux set-hook -t "$scratch_pane_id" pane-died \
 	"swap-pane -s $pane_id -t $scratch_pane_id ; kill-pane -t $scratch_pane_id"
 
